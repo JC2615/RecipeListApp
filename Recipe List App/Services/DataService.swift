bd979d4c89d8be021e66f6854559e996fa466cb2
Joshua Curry <joshuacurry2615@gmail.com>
2021-08-22 19:17:20 -0400

    Added new JSON data and updated model and views to reflect those changes

--- Recipe List App/Services/DataService.swift
+++ Recipe List App/Services/DataService.swift
@@ -22,6 +22,11 @@ class DataService {
                     let recipeData = try decoder.decode([Recipe].self, from: data)
                     for r in recipeData {
                         r.id = UUID()
+                        
+                        // Add uniquie id to recipe ingredients
+                        for i in r.ingredients {
+                            i.id = UUID()
+                        }
                     }
                     return recipeData
                 } catch {
